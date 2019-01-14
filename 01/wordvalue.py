3c08c17676ff9fc4aa3d6d7c5ed4d6ee9ddc79f8
Dan Yeaw <dan@yeaw.me>
2019-01-13 22:53:24 -0500

    Complete solution for challenge 1
    Signed-off-by: Dan Yeaw <dan@yeaw.me>

---
+++ 01/wordvalue.py
@@ -0,0 +1,19 @@
+from data import DICTIONARY, LETTER_SCORES
+
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    with open(DICTIONARY, mode="r") as dictionary:
+        return [line.rstrip("\r\n") for line in dictionary]
+
+
+def calc_word_value(word):
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+def max_word_value(words=load_words()):
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    return max(words, key=calc_word_value)
